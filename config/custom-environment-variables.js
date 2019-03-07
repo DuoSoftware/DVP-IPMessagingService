1f9d39760c2bd98842237a63bc78e2f10557feb7
waruna <waruna@duosoftware.com>
2019-03-07 11:55:42 +0530

    add environment variable

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -50,7 +50,8 @@ module.exports = {
         "port": "HOST_IPMESSANGERSERVICE_PORT",
         "version": "HOST_VERSION",
 		"token": "HOST_TOKEN",
-        "encryptedhex": "HOST_ENCRYPTEDHEX"
+        "encryptedhex": "HOST_ENCRYPTEDHEX",
+        "welcome_message": "HOST_WELCOME_MESSAGE"
     },
 
     "LBServer" : {
