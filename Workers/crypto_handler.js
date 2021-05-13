b579b28efddf8b1e5caff08804a98be04ec400b8
coderaduo <coderaduo@gmail.com>
2021-05-13 10:04:34 +0530

    Update crypto_handler.js

--- Workers/crypto_handler.js
+++ Workers/crypto_handler.js
@@ -1,5 +1,7 @@
 var config = require("config");
 var aesjs = require("aes-js");
+let TextEncoder = require("util").TextEncoder;
+let TextDecoder = require("util").TextDecoder;
 
 /*var key_256 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29, 30, 31];
 var key = Buffer.from(key_256);
